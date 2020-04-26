d3f5fc0c4bcbc433caab524b504d151e527bf38f
16071316 <16071316@isvnu.vn>
2020-04-27 03:00:44 +0700

    update admin add book

---
+++ admin/add-book.php
@@ -0,0 +1,138 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+
+if(isset($_POST['add']))
+{
+$bookname=$_POST['bookname'];
+$category=$_POST['category'];
+$author=$_POST['author'];
+$isbn=$_POST['isbn'];
+$price=$_POST['price'];
+$amount=$_POST['amount'];
+$av = $amount;
+$sql="INSERT INTO  tblbooks(BookName,CategoryName,AuthorName,ISBNNumber,BookPrice,Amount,Available) VALUES(:bookname,:category,:author,:isbn,:price,:amount,:av)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':bookname',$bookname,PDO::PARAM_STR);
+$query->bindParam(':category',$category,PDO::PARAM_STR);
+$query->bindParam(':author',$author,PDO::PARAM_STR);
+$query->bindParam(':isbn',$isbn,PDO::PARAM_STR);
+$query->bindParam(':price',$price,PDO::PARAM_STR);
+$query->bindParam(':amount',$amount,PDO::PARAM_STR);
+$query->bindParam(':av',$av,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$_SESSION['msg']="Book Listed successfully";
+header('location:manage-books.php');
+}
+else 
+{
+$_SESSION['error']="Something went wrong. Please try again";
+header('location:manage-books.php');
+}
+
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Online Library Management System | Add Book</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+      <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+    <div class="content-wra
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line">Add Book</h4>
+                
+                            </div>
+
+</div>
+<div class="row">
+<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3"">
+<div class="panel panel-info">
+<div class="panel-heading">
+Book Info
+</div>
+<div class="panel-body">
+<form role="form" method="post">
+<div class="form-group">
+<label>Book Name<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="bookname" autocomplete="off"  required />
+</div>
+
+<div class="form-group">
+<label> Category<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="category" autocomplete="off"  required />
+</div>
+
+
+<div class="form-group">
+<label> Author<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="author" autocomplete="off"  required />
+</div>
+
+<div class="form-group">
+<label>ISBN Number<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="isbn"  required="required" autocomplete="off"  />
+<p class="help-block">An ISBN is an International Standard Book Number.ISBN Must be unique</p>
+</div>
+
+ <div class="form-group">
+ <label>Price<span style="color:red;">*</span></label>
+ <input class="form-control" type="text" name="price" autocomplete="off"   required="required" />
+ </div>
+
+ <div class="form-group">
+ <label>Amount<span style="color:red;">*</span></label>
+ <input class="form-control" type="text" name="amount" autocomplete="off"   required="required" />
+ </div>
+<button type="submit" name="add" class="btn btn-info">Add </button>
+
+                                    </form>
+                            </div>
+                        </div>
+                            </div>
+
+        </div>
+   
+    </div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+  <?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
+<?php } ?>
